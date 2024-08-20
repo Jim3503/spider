7710739924dab5d9cbd8bf604d684fc2d2af2377
Jim3503 <jiming1920@mails.jlu.edu.cn>
2024-08-20 19:33:52 +0800

    测试一下

--- test.py
+++ test.py
@@ -2,8 +2,10 @@
 Author: Jim3503 jiming1920@mails.jlu.edu.cn
 Date: 2024-08-20 18:20:50
 LastEditors: Jim3503 jiming1920@mails.jlu.edu.cn
-LastEditTime: 2024-08-20 19:13:57
+LastEditTime: 2024-08-20 19:33:02
 FilePath: \spider\test.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 import torch
+import torch.nn as nn
+import torch.optim as optim
